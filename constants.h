1b839cb2afc2fe956b97d3b8416c7b01e9c471cc
Alastair Porter <alastair@porter.net.nz>
2014-10-26 20:49:40 +0100

    Set read website

--- constants.h
+++ constants.h
@@ -2,8 +2,8 @@
 #define FPSUBMIT_CONSTANTS_H_
 
 static const char *API_KEY_URL = "http://acousticbrainz.org";
-//static const char *SUBMIT_URL = "http://acousticbrainz.org/%1/low-level";
-static const char *SUBMIT_URL = "http://127.0.0.1:8080/%1/low-level";
+static const char *SUBMIT_URL = "http://acousticbrainz.org/%1/low-level";
+//static const char *SUBMIT_URL = "http://127.0.0.1:8080/%1/low-level";
 static const int AUDIO_LENGTH = 120;
 static const int MAX_ACTIVE_FILES = 3;
 
